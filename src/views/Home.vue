648d9b0eb8d22afe79d03ab9ae1e1ca16b284868
munene-m <munenenjue18@gmail.com>
2023-06-13 16:16:55 +0300

    view that contains all components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,67 +1,20 @@
 <script setup>
-import BulbIcon from '../components/icons/BulbIcon.vue'
-const tools = [
-{
-    id: 6,
-    name: 'Javascript (ES6+)'
-  },
-  {
-    id:1,
-    name: 'Vue.js'
-  }, 
+import Hero from "../components/Hero.vue";
+import About from "../components/About.vue";
+import Projects from "../components/Projects.vue";
+import Contact from '../components/Contact.vue'
+import Footer from "../components/Footer.vue";
 
-  {
-    id:4,
-    name: 'Node.js'
-  },
-  {
-    id:5,
-    name: 'Express js'
-  },
-  {
-    id:3,
-    name: 'Tailwind CSS'
-  },
-  {
-    id:2,
-    name: 'Nuxt js'
-  }
-]
+// const currentYear = new Date().getFullYear()
+
 </script>
 
 <template>
-  <main class="md:mx-40 mx-10">
-    <section class="pt-16 py-24">
-        <p class="font-mono pb-3 text-cyan-500">Hi my name is</p>
-        <h1 class="font-inter pb-4 text-5xl sm:text-6xl font-bold">MacMillan Munene.</h1>
-        <h2 class="font-inter pb-4 font-bold text-zinc-500 text-4xl sm:text-5xl">I build cool stuff on the web.</h2>
-        <p class="sm:w-1/2 w-full text-lg pb-6">I am a software developer focused on building beautiful and interactive user experiences using tools such as Vue.js, Nuxt and Tailwind as well as RESTful APIs using Node.js.</p>
-        <button class="px-5 py-3 border border-cyan-500 rounded hover:bg-cyan-500 hover:text-zinc-800 transition-all eas duration-200">Check out my resume</button>
-    </section>
-    
-    <section class="pt-28">
-    <h2 class="font-inter text-cyan-500 text-2xl pb-10">About me</h2>
-
-      <article class="grid sm:grid-cols-2 grid-cols-1 gap-10  justify-items-center place-items-center">
-      <img class="md:mr-auto rounded" src="../assets/codingGuy.gif" alt="Animated coding GIF">
-
-
-        <div>
-          <p>I am a <span class="text-cyan-500">MEVN-stack </span>developer based in Nairobi, Kenya. My interest in web development began in 2021 when I first learnt HTML and CSS
-          in my second year of pursuing my Bachelor's degree in Business and IT. Since then I have been creating projects and learning best practices and improving
-          my skills. Here are a couple of technologies I have been working with: 
-        </p>
-        <div class="grid grid-cols-2 gap-4 md:gap-0">
-        <ul class="font-mono pt-4" v-for="tool in tools" :key="tool.id">
-          <li class="flex items-center">
-            <BulbIcon class="text-cyan-500"/> {{ tool.name }}
-          </li>
-        </ul>
-      </div>
-        </div>
-
-      </article>
-
-    </section>
+  <main class="lg:mx-32 sm:mx-12 mx-8">
+    <Hero />
+    <About />
+    <Projects/>
+    <Contact/>
   </main>
+<Footer />
 </template>
