b81863781d4a9068e2a23b7b941405b60600b474
munene-m <munenenjue18@gmail.com>
2023-06-13 16:13:48 +0300

    initialized aos

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { RouterView } from "vue-router";
 import Navbar from "./components/Navbar.vue";
+import { onMounted } from 'vue';
+import AOS from "aos";
+onMounted(() => {
+  AOS.init();
+});
 </script>
 
 <template>
