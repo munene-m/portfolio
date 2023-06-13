95b9e164f57bb787cb6e2ce1efce6e918a26d039
munene-m <munenenjue18@gmail.com>
2023-06-13 16:15:51 +0300

    svg icons installed

---
+++ src/components/icons/LinkIcon.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M11 4H4v14a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-5M9 15L20 4m-5 0h5v5"></path></svg>
+</template>
+
+<script>
+export default {
+  name: 'IconamoonLinkExternalBold'
+}
+</script>
