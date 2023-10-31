ddcd50e01b4fe3d5d28692382b507a46c1595e02
munene-m <munenenjue18@gmail.com>
2023-10-31 15:14:40 +0300

    added linked in to the footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,17 +1,35 @@
 <script setup>
-import GithubIcon from './icons/GithubIcon.vue';
-import GmailIcon from './icons/GmailIcon.vue';
+import GithubIcon from "./icons/GithubIcon.vue";
+import GmailIcon from "./icons/GmailIcon.vue";
+import LinkedInIcon from "./icons/LinkedInIcon.vue";
 </script>
 
 <template>
-  <footer class="flex justify-center flex-col rounded-t-md items-center py-2 bg-[#252525] text-sm md:text-base">
+  <footer
+    class="flex justify-center flex-col rounded-t-md items-center py-2 bg-[#252525] text-sm md:text-base"
+  >
     <p class="flex items-center text-center">Built by Munene.</p>
-    <div class="grid grid-cols-2 gap-2 pt-1 place-items-center">
-      <a href="https://github.com/munene-m" aria-label="View my Github page"
+    <div class="grid grid-cols-3 gap-2 pt-1 place-items-center">
+      <a
+        href="https://github.com/munene-m"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="View my Github page"
         ><GithubIcon class="transition-all duration-300 hover:text-cyan-500"
       /></a>
-      <a href="mailto:munenenjue18@gmail.com" aria-label="Reach out to me on email"
+      <a
+        href="mailto:munenenjue18@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Reach out to me on email"
         ><GmailIcon class="transition-all duration-300 hover:text-cyan-500"
+      /></a>
+      <a
+        href="https://www.linkedin.com/in/macmillan-munene-b97235204/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Link to my linkedIn"
+        ><LinkedInIcon class="transition-all duration-300 hover:text-cyan-500"
       /></a>
     </div>
   </footer>
